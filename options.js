d8b7a6db322028ca69b7a90ad260c11934fbb8e7
Scott Busche <swbusche@gmail.com>
2016-10-26 12:40:12 -0500

    Move from LocalStorage to Chrome Storage
    Chrome Storage appears to be the preferred way to save options since
    Chrome 20.
    This also handles migrating from LocalStorage to Chrome Storage.
    
    Will need to add a new version check in the future, as the current one is
    always false and popping up the options.

--- options.js
+++ options.js
@@ -1,42 +1,40 @@
-// JavaScript Document
-	document.getElementById("saveButton").addEventListener('click',save_options)
+function restore_options() {
+	chrome.storage.sync.get({
+		rtcURL: '',
+		pointsIndicator: false,
+		burndownIndicator: false,
+		rrcFullscreenIndicator: false
+	}, function (options) {
+		document.getElementById('rtcURL').value = options.rtcURL;
+		document.getElementById('pointsIndicatorCheckbox').checked = options.pointsIndicator;
+		document.getElementById('burndownCheckbox').checked = options.burndownIndicator;
+		document.getElementById('rrcFullscreenCheckbox').checked = options.rrcFullscreenIndicator;
+	});
+}
 
-	if (localStorage["rtcURL"]){
-		document.getElementById("rtcURL").value = localStorage["rtcURL"];
-	}	
+function save_options() {
+	var rtcURL = document.getElementById('rtcURL');
+	chrome.storage.sync.set({
+		rtcURL: rtcURL.value.trim(),
+		pointsIndicator: document.getElementById('pointsIndicatorCheckbox').checked,
+		burndownIndicator: document.getElementById('burndownCheckbox').checked,
+		rrcFullscreenIndicator: document.getElementById('rrcFullscreenCheckbox').checked
+	}, function () {
+		var status = document.getElementById('status');
+		status.textContent = 'Settings saved successfully.';
 
-	if (localStorage["pointsIndicator"]=='true'){
-		document.getElementById("pointsIndicatorCheckbox").checked = true;
-	}	
-
-	if (localStorage["burndownIndicator"]=='true'){
-		document.getElementById("burndownCheckbox").checked = true;
-	}	
-
-	if (localStorage["rrcFullscreenIndicator"]=='true'){
-		document.getElementById("rrcFullscreenCheckbox").checked = true;
-	}	
-
-	function save_options() {
-		var name = document.getElementById("rtcURL");
-		name.value = name.value.trim();
-		localStorage["rtcURL"] = name.value;
-		localStorage["pointsIndicator"] = document.getElementById('pointsIndicatorCheckbox').checked;
-		localStorage["burndownIndicator"] = document.getElementById('burndownCheckbox').checked;
-		localStorage["rrcFullscreenIndicator"] = document.getElementById('rrcFullscreenCheckbox').checked;
-
-		var status = document.getElementById("status");
-		status.innerHTML = "Settings saved successfully.";
-
-		if (typeof statusTimer != "undefined") {
-			status.innerHTML = "";
-			setTimeout(function() {
-				status.innerHTML = "Settings saved successfully.";
+		if (typeof statusTimer !== 'undefined') {
+			status.textContent = '';
+			setTimeout(function () {
+				status.textContent = 'Settings saved successfully.';
 			}, 50);
 			clearTimeout(statusTimer);
 		}
-		statusTimer = setTimeout(function() {
-			status.innerHTML = "";
+		statusTimer = setTimeout(function () {
+			status.textContent = '';
 		}, 3000);
-	}
-	
+	});
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('saveButton').addEventListener('click', save_options);
